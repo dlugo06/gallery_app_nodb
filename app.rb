a816358c2d35a1b034e8a1f94acf6d917305d12f
Diego Lugo <diego.lugo.06@gmail.com>
2016-04-07 15:41:18 -0700

    Connect db, set routes with models

--- app.rb
+++ app.rb
@@ -6,6 +6,21 @@ class Image < ActiveRecord::Base
 
 end
 
-get '/' do
-  "Hello Sinatra"
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database =>  'gallery_dev.sqlite3.db'
+)
+
+
+
+get "/" do
+  Image.create({url: "http://placekitten.com/200/300", title: "The first magic picture"})
+  @images = Image.all
+  erb :index
+end
+
+get "/photo/:id" do
+  @images = Image.all
+  erb :single
 end
