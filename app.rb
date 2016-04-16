c8be2d34cd99ff23f0f6254ce80fab613067a81b
Diego Lugo <diego.lugo.06@gmail.com>
2016-04-15 17:32:05 -0700

    Add new image button

--- app.rb
+++ app.rb
@@ -5,8 +5,14 @@ require_relative 'models/image'
 require_relative 'config/environments'
 
 
+post '/' do
+  Image.create({url: "http://i150.photobucket.com/albums/s85/michelleNpete/BaBas/awesome-beautiful-blue-eyes-cat-cute-Favimcom-110476.jpg", title: "The first magic picture"})
+  @images = Image.all
+  erb :index
+end
+
 get "/" do
-  Image.create({url: "http://placekitten.com/200/300", title: "The first magic picture"})
+  # Image.create({url: "http://placekitten.com/200/300", title: "The first magic picture"})
   @images = Image.all
   erb :index
 end
--- models/image.rb
+++ models/image.rb
@@ -1,3 +1,2 @@
 class Image < ActiveRecord::Base
-
 end
